a95720e12d1c6947cc192788b7c3c68f2510e84f
konstrictor <jonkur96@gmail.com>
2020-09-09 18:01:00 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    En construction
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+};
+</script>
+
+<style>
+</style>
