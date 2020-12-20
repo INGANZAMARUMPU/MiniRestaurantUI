6020138f10a7a6fae9b35b641e7d559c24a32d9e
konstrictor <jonkur96@gmail.com>
2020-12-20 18:37:23 +0200

    redesigned usercard

--- src/components/person.vue
+++ src/components/person.vue
@@ -21,4 +21,27 @@ export default {
 };
 </script>
 <style scoped>
+.user-card{
+	text-align: center;
+	width: 140px;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+	padding: 10px 5px;
+	margin: 10px;
+	background-color: #fff;
+	border-radius: 5px;
+}
+.user-card:hover{
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+.img-user{
+	width: 100px;
+	height: 100px;
+	border-radius: 50%;
+	background-color: #ddd;
+	overflow: hidden;
+	margin: auto;
+}
+.img-user img{
+	width: 100%;
+}
 </style>
